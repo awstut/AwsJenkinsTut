8c3346498a857fdb2caea15c24deee3db2cf3df7
josephmfaulkner <josephmfaulkner@gmail.com>
2019-04-20 23:16:13 -0700

    fix

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-[[-e /etc/init/helloworld.conf]] && status helloworld | grep -q '^helloworld start/running, process' && [[ $? -eq 0]] && stop helloworld || echo "Application Not Started"
+    
+[[ -e /etc/init/helloworld.conf ]] \
+  && status helloworld | \
+    grep -q '^helloworld start/running, process' \
+  && [[ $? -eq 0 ]] \
+  && stop helloworld || echo "Application not started"  

8a71f67466c1faa441e27d1a62b0254b5a47a9cb
josephmfaulkner <josephmfaulkner@gmail.com>
2019-04-21 11:37:31 -0700

    Test CI Pipeline

--- app/helloworld.js
+++ app/helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end("Hello World, it's nice to meet cha!")
+   response.end("CI Pipeline")
 }).listen(3000)
 
 // Console will print the message
